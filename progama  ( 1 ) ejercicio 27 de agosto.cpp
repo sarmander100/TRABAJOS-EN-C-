221f48644ad49ccc843dd180c54f3207da1fbb3f
anderson sarmiento <53631767+sarmander100@users.noreply.github.com>
2019-08-27 21:41:42 -0500

    Add files via upload

---
+++ progama  ( 1 ) ejercicio 27 de agosto.cpp
@@ -0,0 +1,18 @@
+// TRABAJO EN CLASE ( 1 ) 27 DE AGOSTO //
+// ANDERSON SARMIENTO //
+
+# include <iostream>  // esto es una libreria de c++ //
+# include <conio.h>
+# include <cstdlib>
+using namespace std;
+main()
+
+{
+	cout << "BUENAS NOCHES ANDERSON";
+	cout << endl;   //esto es  como si fuera un llamar nueva_linea en lpp o para darle enter//
+	
+	getch ();
+	system ("pause");
+	cout << "  ES HORA DE IRNOS PARA LA CASA";
+}
+
